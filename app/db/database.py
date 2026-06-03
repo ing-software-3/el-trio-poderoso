ce597c1d1a1098f0c4c445836d8295990391dd61
Laura Delgado Vargas <tu_correo@example.com>
2026-06-02 21:20:05 -0500

    refactor:actualizacion de errores

--- app/db/database.py
+++ app/db/database.py
@@ -1,8 +1,9 @@
 import os
-from dotenv import load_dotenv
 from pathlib import Path
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
@@ -12,6 +13,11 @@ load_dotenv(dotenv_path=env_path)
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+if DATABASE_URL is None:
+    raise ValueError(
+        "DATABASE_URL no está configurada en el archivo .env"
+    )
+
 print("DATABASE_URL:", DATABASE_URL)
 
 engine = create_engine(DATABASE_URL)
@@ -19,10 +25,11 @@ engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
-    bind=engine
+    bind=engine,
 )
 
 Base = declarative_base()
+
 
 def get_db():
     db = SessionLocal()
